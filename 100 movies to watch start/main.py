51b2e771de4442b52e4bc9fff0653c8d99894a10
jinicha <jinichaa@gmail.com>
2021-11-24 01:18:15 -0800

    Reverse the list

--- 100 movies to watch start/main.py
+++ 100 movies to watch start/main.py
@@ -8,5 +8,7 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 titles = soup.findAll(name="h3", class_="title")
 title_list = [title.get_text() for title in titles]
-
+title_list = title_list[::-1]
+for title in title_list:
+    print(title)
 # print(title_list)
